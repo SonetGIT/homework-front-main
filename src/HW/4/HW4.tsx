c2f48b85a7d77a69a62cec6504824a3b511e1882
Sonet <sonetsaparbekova@gmail.com>
2023-11-21 10:56:08 +0600

    21112023

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -3,6 +3,7 @@ import s from './HW4.module.css';
 import { Button } from './Button';
 import { Input } from './Input';
 
+
 export const HW4 = () => {
   // 1️⃣ Раскомментируйте JSX (HW4.tsx) и вы увидите,
   // что приложение начнет гореть красным и ругаться 😡
@@ -24,27 +25,29 @@ export const HW4 = () => {
 
   return (
     <div id={'hw04'}>
-      {/*{currentText ? (*/}
-      {/*  <h1 id={'hw04-text'}>{currentText}</h1>*/}
-      {/*) : (*/}
-      {/*  <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>*/}
-      {/*)}*/}
+      {currentText ? (
+        <h1 id={'hw04-text'}>{currentText}</h1>
+      ) : (
+        <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>
+      )}
 
-      {/*<Input НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      {/* <Input НЕ ХВАТАЕТ АРГУМЕНТОВ /> */}
+      <Input currentText={currentText} setCurrentText={setCurrentText} />
 
-      {/*<Button НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      <Button name={'+'} callBack={handleSave} />
 
-      {/*<h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>*/}
+      <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-      {/*<ol id={'hw04-tasks'}>*/}
-      {/*  {texts.map((el, index) => {*/}
-      {/*    return (*/}
-      {/*      <li key={index} id={`hw04-task-${index}`} className={ЕСЛИ ЧЕТН ? S.chetNechet : ''}>*/}
-      {/*        {el}*/}
-      {/*      </li>*/}
-      {/*    );*/}
-      {/*  })}*/}
-      {/*</ol>*/}
+      <ol id={'hw04-tasks'}>
+       {texts.map((el, index) => {
+         return (
+          // bool === true ? 'true' : 'false'
+            <li key={index} id={`hw04-task-${index}`} className={index % 2  ? s.chetNechet : ''}> 
+              {el}                                               
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 };
